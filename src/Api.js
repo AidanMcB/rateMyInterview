3b33856f1c29343c34366481f2c0eb0f55e29529
Faizah Ahsan <faizah1992@gmail.com>
2020-11-07 13:14:58 -0600

    change

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { serve } from '@triframe/arbiter'
 import path from 'path'
 
@@ -6,13 +5,4 @@ serve(path.resolve(__dirname, './models'), {
     session: {
 
     }
-=======
-import { serve } from '@triframe/arbiter'
-import path from 'path'
-
-serve(path.resolve(__dirname, './models'), {
-    session: {
-
-    }
->>>>>>> 03383276e5cd4151ae096938bacae2260b779375
 })
