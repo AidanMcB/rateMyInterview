dfde47e4bebaa3d249f804c70c15ba1ff6a07edb
melkilic <melike180400@gmail.com>
2020-11-07 10:05:40 -0600

    first commit

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,22 +1,21 @@
-import { Resource } from '@triframe/core'
-import { Model, string, include }  from '@triframe/scribe'
-import { belongsTo } from '@triframe/scribe/dist/decorators'
+/** @format */
+
+import { Resource } from "@triframe/core";
+import { Model, string, include } from "@triframe/scribe";
+import { belongsTo } from "@triframe/scribe/dist/decorators";
 
 export class Review extends Resource {
+  @include(Model)
+  @string
+  title = "";
 
-    @include(Model)
+  @integer
+  rating = 4;
 
-    @string
-    title = "" 
-    
-    @integer
-    rating = ""
+  @string
+  description = "";
 
-    @string
-    description = ""
-
-    @belongsTo({a: "User", a: "Company"})
-    user = null
-    company = null
-
+  @belongsTo({ a: "User", a: "Company" })
+  user = null;
+  company = null;
 }
