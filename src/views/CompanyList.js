0e1acff4ee4f6dd2d97e28af062d6a889bfaeeab
franciscosierra1915 <fransierra.s.15@gmail.com>
2020-11-08 16:11:45 -0600

    company list

--- src/views/CompanyList.js
+++ src/views/CompanyList.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import { Paragraph, tether, List, TextInput, Container, Heading } from '@triframe/designer'
+import {
+    Section,
+    Button,
+    Subheading,
+    Appbar,
+    Card,
+    Surface,
+    Column,
+    Grid,
+    Right
+  } from "@triframe/designer";
 
 export const CompanyList = tether(function* ({ Api, redirect }) {
 
@@ -26,17 +37,32 @@ export const CompanyList = tether(function* ({ Api, redirect }) {
     }
 
     return (
-        <Container>
-            <Heading>Company List</Heading>
-
-            <TextInput label="Find a Company" onChange={(value) => handleSearch(value)} />
-
+        <Container
+        style={{ backgroundColor: "#191970", padding: "20px", height: "90vh" }}>
+        <Surface>
+            <Heading
+            size="large"
+            style={{ backgroundColor: "#00dbc4", padding: "10px" }}
+            >Company List
+            </Heading>
+        </Surface>
+        
+            <TextInput label="Find a Company" onChange={(value) => handleSearch(value)} style={{ backgroundColor: "#00BFFF", marginTop: "10px", padding: "10px", maxWidth: "500px", display: "table", margin: "auto" }}/>
+            <Grid gutter={3} base={3}>
             {state.companyList.map(company => (
-                <List.Item
-                    title={company.name}
-                    onPress={() => redirect(`/companies/${company.id}`)} />
+                        <Column key={company.id}>
+                        <Card style={{ height: "70px", backgroundColor: "#ddedff"}}>
+                          <List.Item
+                            style={{ textOvereflow: "ellipsis"}}
+                            title={company.name}
+                            description={company.location}
+                            onPress={() => redirect(`/companies/${company.id}`)}
+                          ></List.Item>
+                        </Card>
+                      </Column>
 
-            ))}
+            ))}</Grid>
         </Container>
     )
 })
+
